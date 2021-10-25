3f8d5bf530e67763327d627a5bfe1be0b3f5b83b
snakeshaker <brabus870@gmail.com>
2021-10-25 22:38:24 +0600

    Сделал блок поиска по критериям из дз

--- vue-kolesa-upgrade/src/components/Search.vue
+++ vue-kolesa-upgrade/src/components/Search.vue
@@ -3,7 +3,13 @@
     <button class="search__icon" type="button">
       <img src="@/assets/svg/search-big.svg" alt="Search">
     </button>
-    <input class="search__input" id="search" type="text" placeholder="Поиск">
+    <input
+      class="search__input"
+      v-model="search"
+      id="search"
+      type="text"
+      placeholder="Поиск"
+    >
   </label>
 </template>
 
@@ -11,5 +17,15 @@
 
 export default {
   name: 'Search',
+  data() {
+    return {
+      search: '',
+    };
+  },
+  methods: {
+    setSearch(e) {
+      this.search = e.target.value;
+    },
+  },
 };
 </script>
