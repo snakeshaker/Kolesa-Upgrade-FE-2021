216fcc94b9cf14b6c36576261e68204e4694cf49
snakeshaker <brabus870@gmail.com>
2021-10-25 22:11:53 +0600

    Создал компонент подвала

--- vue-kolesa-upgrade/src/App.vue
+++ vue-kolesa-upgrade/src/App.vue
@@ -72,35 +72,7 @@
         </main>
       </div>
     </div>
-    <footer class="footer">
-        <div class="footer__wrapper">
-            <div class="footer__socials">
-                <div class="footer__credit">
-                    © Kolesa Group
-                </div>
-                <ul>
-                    <li><a href="https://www.instagram.com/kolesagroup/" target="_blank">
-                        <img src="@/assets/svg/social-instagram.svg" alt="Instagram">
-                    </a></li>
-                    <li><a href="https://www.youtube.com/channel/UC6o6waSdIvmIdvy7qTqxY9A" target="_blank">
-                        <img src="@/assets/svg/social-youtube.svg" alt="YouTube">
-                    </a></li>
-                    <li><a href="https://vk.com/kolesakrishamarket" target="_blank">
-                        <img src="@/assets/svg/social-vk.svg" alt="VK">
-                    </a></li>
-                </ul>
-            </div>
-            <div class="footer__feedback">
-                <div class="footer__info">
-                    Есть идеи что улучшить? <br>
-                    Не знаешь, с кем решить проблему?
-                </div>
-                <button class="footer__btn" type="button">
-                    Написать
-                </button>
-            </div>
-        </div>
-    </footer>
+    <app-footer></app-footer>
     <modal
       :data="modalData"
       :balance="balance"
@@ -125,6 +97,7 @@ import OrderModal from './components/OrderModal.vue';
 import Search from './components/Search.vue';
 import SideNavigation from './components/sideNavigation.vue';
 import User from './components/User.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
@@ -135,6 +108,7 @@ export default {
     SideNavigation,
     HotButtons,
     OrderModal,
+    AppFooter,
   },
   data() {
     return {
---
+++ vue-kolesa-upgrade/src/components/AppFooter.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrapper">
+      <div class="footer__socials">
+        <div class="footer__credit">
+          © Kolesa Group
+        </div>
+        <ul>
+          <li><a href="https://www.instagram.com/kolesagroup/" target="_blank">
+            <img src="@/assets/svg/social-instagram.svg" alt="Instagram">
+          </a></li>
+          <li><a href="https://www.youtube.com/channel/UC6o6waSdIvmIdvy7qTqxY9A" target="_blank">
+            <img src="@/assets/svg/social-youtube.svg" alt="YouTube">
+          </a></li>
+          <li><a href="https://vk.com/kolesakrishamarket" target="_blank">
+            <img src="@/assets/svg/social-vk.svg" alt="VK">
+          </a></li>
+        </ul>
+      </div>
+      <div class="footer__feedback">
+        <div class="footer__info">
+          Есть идеи что улучшить? <br>
+          Не знаешь, с кем решить проблему?
+        </div>
+        <button class="footer__btn" type="button">
+          Написать
+        </button>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+};
+</script>
