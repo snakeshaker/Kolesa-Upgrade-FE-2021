ed5b7c7f7346f9e223c7acc0c7c22093574ea1bc
snakeshaker <brabus870@gmail.com>
2021-11-01 21:40:44 +0600

    Установил router

--- vue-kolesa-upgrade/src/App.vue
+++ vue-kolesa-upgrade/src/App.vue
@@ -1,186 +1,32 @@
 <template>
   <div id="app">
-    <div class="wrap">
-      <div class="content-container">
-        <header class="header">
-            <div class="header__logo">
-                <img src="@/assets/svg/kolesa-logo.svg" alt="Logo">
-            </div>
-            <form action="" class="header__form">
-              <search></search>
-            </form>
-            <user @updateUser="updateUser" :appUser="user"></user>
-        </header>
-        <side-navigation></side-navigation>
-        <main class="main">
-          <div class="main__banner"></div>
-          <hot-buttons></hot-buttons>
-          <div class="main__item-filter">
-            <catalog-filter
-              v-for="(tab,index) in filterTabs"
-              :key="index"
-              :tab="tab"
-              @rAll="renderAll"
-              @rClothes="renderClothes"
-              @rAccessories="renderAccessories"
-            ></catalog-filter>
-          </div>
-          <div class="catalog">
-            <catalog-item
-              v-for="item in renderCatalog"
-              :key="item.id"
-              :item="item"
-              :data="modalData"
-              @openCard="openCard(item)"
-            ></catalog-item>
-          </div>
-        </main>
-      </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <app-footer></app-footer>
-    <modal
-      :data="modalData"
-      :balance="user.score"
-      :isModalOpen="isShowModal"
-      @toggle="toggleModal"
-      @order="setBalance"
-      :uniq="uniqImages"
-    ></modal>
-    <order-modal
-      :data="modalData"
-      :balance="user.score"
-      :isComplete="isComplete"
-      :isOrderPlaced="isShowOrder"
-      @toggle-order="toggleOrderModal"
-    ></order-modal>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import axios from '@/axios';
-import HotButtons from './components/HotButtons.vue';
-import Modal from './components/Modal.vue';
-import OrderModal from './components/OrderModal.vue';
-import Search from './components/Search.vue';
-import SideNavigation from './components/sideNavigation.vue';
-import User from './components/User.vue';
-import AppFooter from './components/AppFooter.vue';
-import CatalogItem from './components/CatalogItem.vue';
-import CatalogFilter from './components/CatalogFilter.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Modal,
-    Search,
-    User,
-    SideNavigation,
-    HotButtons,
-    OrderModal,
-    AppFooter,
-    CatalogItem,
-    CatalogFilter,
-  },
-  data() {
-    return {
-      isShowModal: false,
-      isShowOrder: false,
-      isComplete: false,
-      user: {},
-      modalData: {},
-      uniqImages: [],
-      clothes: [],
-      accessories: [],
-      filterTabs: [
-        {
-          title: 'Все товары',
-          id: 'item_all',
-        },
-        {
-          title: 'Одежда',
-          id: 'item_clothes',
-        },
-        {
-          title: 'Аксессуары',
-          id: 'item_accessories',
-        },
-      ],
-      renderCatalog: [],
-    };
-  },
-  computed: {
-    allItems() {
-      return [...this.clothes, ...this.accessories];
-    },
-  },
-  created() {
-    this.fetchInfo();
-  },
-  mounted() {
-    document.querySelector('.catalog').classList.add('catalog--loading');
-  },
-  methods: {
-    openCard(data) {
-      this.toggleModal();
-      this.uniqImages = [...new Set(data.images)];
-      this.modalData = data;
-    },
-    toggleModal() {
-      this.isShowModal = !this.isShowModal;
-    },
-    toggleOrderModal() {
-      this.isShowOrder = !this.isShowOrder;
-    },
-    sortCatalog(arr) {
-      return arr.slice().sort((item) => (item.isNew !== true ? 1 : -1));
-    },
-    async getAccessories() {
-      axios.get('q3OPxRyEcPvP/data')
-        .then((response) => {
-          this.accessories = this.sortCatalog(response.data);
-        });
-    },
-    async getClothes() {
-      axios.get('-_RLsEGjof6i/data')
-        .then((response) => {
-          this.clothes = this.sortCatalog(response.data);
-        });
-    },
-    async fetchInfo() {
-      await this.getClothes();
-      await this.getAccessories();
-      setTimeout(() => {
-        document.querySelector('.catalog').classList.remove('catalog--loading');
-        this.renderAll();
-      }, 1000);
-    },
-    renderAll() {
-      this.renderCatalog = this.sortCatalog(this.allItems);
-    },
-    renderClothes() {
-      this.renderCatalog = this.clothes;
-    },
-    renderAccessories() {
-      this.renderCatalog = this.accessories;
-    },
-    setBalance(price) {
-      this.toggleModal();
-      if (this.user.score < price) {
-        this.isShowOrder = true;
-        this.isComplete = false;
-      } else {
-        this.isShowOrder = true;
-        this.isComplete = true;
-        this.user.score -= price;
-      }
-    },
-    updateUser(info) {
-      this.user = info;
-    },
-  },
-};
-</script>
-
 <style lang="scss">
-@import './assets/scss/style.scss';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
